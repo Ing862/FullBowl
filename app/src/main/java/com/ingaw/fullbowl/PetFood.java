4d2f091c7d80070ed7d8d5d5408f36ee09c8c1ff
Inga Wojtulewicz <inga_2003@op.pl>
2025-01-28 05:33:49 +0100

    Skończona klasa jedzenia

--- app/src/main/java/com/ingaw/fullbowl/PetFood.java
+++ app/src/main/java/com/ingaw/fullbowl/PetFood.java
@@ -11,7 +11,7 @@ public class PetFood {
     private int id;
 
     @ColumnInfo(name = "pet_food_name")
-    private String Name;
+    private String name;
 
     @ColumnInfo(name = "pet_food_weight")
     private int weight;
@@ -19,13 +19,57 @@ public class PetFood {
     @ColumnInfo(name = "pet_food_brand")
     private String brand;
 
-    
+    @ColumnInfo(name = "pet_food_photo_path")
+    private String imagePath;
 
-
-    public PetFood(String brand, int weight, String name, int id) {
+    public PetFood(int id, String imagePath, String brand, int weight, String name) {
+        this.id = id;
+        this.imagePath = imagePath;
         this.brand = brand;
         this.weight = weight;
-        Name = name;
+        this.name = name;
+    }
+
+    public PetFood() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
