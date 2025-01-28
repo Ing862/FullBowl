21ae291908e3717e2090ee835604e38eaecf0359
Inga Wojtulewicz <inga_2003@op.pl>
2025-01-28 05:43:19 +0100

    Klasy Dao i Database

---
+++ app/src/main/java/com/ingaw/fullbowl/PetFoodDAO.java
@@ -0,0 +1,21 @@
+package com.ingaw.fullbowl;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface PetFoodDAO {
+    @Insert
+    void insert(PetFood petFood);
+
+    @Delete
+    void delete(PetFood petFood);
+
+    @Query("SELECT * FROM pet_food")
+    LiveData<List<PetFood>> getAllFood();
+}
---
+++ app/src/main/java/com/ingaw/fullbowl/PetFoodDatabase.java
@@ -0,0 +1,28 @@
+package com.ingaw.fullbowl;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {PetFood.class},version = 1)
+public abstract class PetFoodDatabase extends RoomDatabase {
+
+    public abstract PetFoodDAO getPetFoodDAO();
+
+    //Singleton
+    private static PetFoodDatabase dbInstance;
+
+    public static synchronized PetFoodDatabase getInstance(Context context){
+        if(dbInstance == null){
+            dbInstance = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            PetFoodDatabase.class,
+                            "pet_food_db").
+                    fallbackToDestructiveMigration()
+                    .build();
+        }
+        return dbInstance;
+    }
+}
